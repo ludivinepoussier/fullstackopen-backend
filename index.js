a1e90378bf63a532dcd5d7d1b6de03d26f26dbea
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-07-24 12:44:30 +0100

    3.5: Phonebook backend step5
    new phonebook entries can be added by making HTTP POST requests, test with REST client

--- index.js
+++ index.js
@@ -62,6 +62,33 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.num) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        num: body.num,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
