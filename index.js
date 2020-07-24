035f250ce87b20945b9f4cf7a5a2cdfab2a51ad3
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-07-24 12:13:16 +0100

    3.3: Phonebook backend step3
    display the information for a single phonebook entry with id and if the id does not exist return a 404 status

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "name": "Arto Hellas",
@@ -40,6 +42,17 @@ app.get('/info', (request, response) => {
         <br/>
         <br/>
         ${date}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(note => note.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 const PORT = 3001
