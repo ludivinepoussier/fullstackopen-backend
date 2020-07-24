a5dcb29dc363f3be557398f1b4d42f5ee91b158f
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-07-24 12:02:13 +0100

    3.2: Phonebook backend step2
    info page show the time of the request and the actual number of entries in the phonebook

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const date = new Date()
+    // response.send(`Phonebook has info for ${persons.length} people`)
+    response.send(
+        `Phonebook has info for ${persons.length} people. 
+        <br/>
+        <br/>
+        ${date}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
